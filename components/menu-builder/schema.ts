e27b8cc7c4948794aa6dfab39ef62c872aadb424
A Khan <abdul.khan@live.se>
2026-02-26 13:11:03 +0100

    refactor(MenuItemForm): add letter capitalization function to schema

--- components/menu-builder/schema.ts
+++ components/menu-builder/schema.ts
@@ -1,7 +1,8 @@
+import { capitalizeFirstLetter } from '@/lib/functions';
 import { z } from 'zod';
 
 export const menuItemFormSchema = z.object({
-  name: z.string().trim().min(1, 'Name is required.'),
+  name: z.string().trim().min(1, 'Name is required.').transform(capitalizeFirstLetter),
   price: z
     .string()
     .refine((s) => s.trim() !== '' && !Number.isNaN(Number(s)), {
@@ -10,7 +11,7 @@ export const menuItemFormSchema = z.object({
     .refine((s) => Number(s) >= 0, {
       message: 'Price must be greater than or equal to 0.',
     }),
-  category: z.string().trim().min(1, 'Category is required.'),
+  category: z.string().trim().min(1, 'Category is required.').transform(capitalizeFirstLetter),
 });
 
 export type MenuItemFormValid = z.output<typeof menuItemFormSchema>;
